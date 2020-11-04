d1176ce518d6f07cc585d105562b665670ce599f
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-11-04 20:10:29 +0900

    Update index.js
    Import Middlewares

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 // index.js
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Example app listening on port ${PORT}!`);
-}
+//Looking Middleware
+const betweenHome = (req, res, next) => {
+  console.log("중간에 있어요!");
+  next();
+};
+const handleHome = (req, res) => res.send("홈 화면입니다!!ㅎㅎ");
+const handleProfile = (req, res) => res.send("프로필 화면입니다.");
 
-function handleHome(req, res) {
-  // 누가 어떤 페이지를 요청했는지, 어떤 종류의 데이터가 페이지로 전송됐는지 확인하려면 request object
-  console.log(req);
-  //응답할 내용을 설정할 땐 response object
-  res.send("홈 화면입니다.");
-}
-
-function handleProfile(req, res) {
-  console.log(req);
-  res.send("프로필 화면입니다.");
-}
-
+//어떤 루트로 들어오든 미들웨어가 실행되도록 하려면
+//위에서 아래로 차례로 함수들이 실행되도록
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan(helmet()));
 app.listen(PORT, handleListening);
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
